34122fd320ded6d2a9c86b487856650be4577978
akobey <akobey@gmail.com>
2021-03-29 14:50:22 +0300

    Firts Commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div className="bg-pink-200">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <main
+
+                className="bg-pink-200">
+
+                <h1>Akobey</h1>
+
+            </main>
+
+
+        </div>
+    )
+}
